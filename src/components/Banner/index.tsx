8718f241d1cd44b5894cbd061d5fa83300d588b1
Ubirata Lucas dos Santos Antunes <ubirata.antunes@cieepr.org.br>
2023-02-03 13:22:34 -0300

    tailwind adicionado

---
+++ src/components/Banner/index.tsx
@@ -0,0 +1,11 @@
+import './Banner.css';
+
+export default function Banner() {
+
+    return (
+        <header className='banner'>
+            <img src="/images/bannerWZ.jpg" alt="Banner COD Warzone 2.0" />
+            <p>Find Warzone players here!</p>
+        </header>
+    )
+}
---
+++ src/components/Form/index.tsx
@@ -0,0 +1,9 @@
+import './Form.css';
+
+export default function Form () {
+    return (
+        <div>
+            FORM
+        </div>
+    )
+}
