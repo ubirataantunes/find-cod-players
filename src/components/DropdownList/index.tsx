ef71f074454e24725631f5be30d71e64841abfbe
Ubirata Lucas dos Santos Antunes <ubirata.antunes@cieepr.org.br>
2023-03-01 12:54:30 -0300

    Consumindo API

--- src/components/DropdownList/index.tsx
+++ src/components/DropdownList/index.tsx
@@ -9,10 +9,10 @@ const DropdownList = (props: any) => {
         onChange={(e) => props.changed(e.target.value)}
         value={props.value}
       >
-        <option value="">Select your platform</option>
-        {props.platform.map((item: any) => (
-          <option key={item}>{item}</option>
-        ))}
+        <option value="">Select platform</option>
+        <option value="battle">Battle.net</option>
+        <option value="xbl">Xbox Live</option>
+        <option value="psn">PlayStation Network</option>
       </select>
     </div>
   );
--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -9,58 +9,91 @@ import InputField from "../InputField";
 // import DropdownList from '../DropdownList';
 import Button from "../Button";
 import DropdownList from "../DropdownList";
+import axios from "axios";
+
+type User = {
+  username: string;
+  platform: string;
+  level: number;
+  kdRatio: number;
+  wins: number;
+  gamesPlayed: number;
+  winPercentage: number;
+};
+
+const API_BASE_URL = "https://api.tracker.gg/api/v2/warzone/";
+
+async function searchUsers(username: string, platform: string): Promise<User[]> {
+  const response = await axios.get(`${API_BASE_URL}/standard/profile/${platform}/${username}`);
+  const { data } = response.data;
+
+  return data.segments.map((segment: any) => ({
+    username: segment.attributes.displayName,
+    platform: segment.attributes.platformSlug,
+    level: segment.stats.level.value,
+    kdRatio: segment.stats.kdRatio.value,
+    wins: segment.stats.wins.value,
+    gamesPlayed: segment.stats.gamesPlayed.value,
+    winPercentage: segment.stats.wins.value / segment.stats.gamesPlayed.value
+  }));
+}
 
 const Form = (props: any) => {
   // states
-  const [name, setName] = useState("");
-  const [ID, setID] = useState("");
-  const [team, setTeam] = useState("");
+  const [username, setUsername] = useState("");
+  const [platform, setPlatform] = useState("");
+  const [users, setUsers] = useState<User[]>([]);
 
-  const handleSubmit = (e: any) => {
+  const handleSearch = async (e: any) => {
     e.preventDefault();
     // sending professional data to App.js state
     props.registeredPlayers({
-      name,
-      ID,
-      team,
+      username,
+      platform,
     });
-    setName("");
-    setID("");
-    setTeam("");
+    setUsername("");
+    setPlatform("");
+
+    const result = await searchUsers(username, platform);
+    setUsers(result);
   };
 
-  const platform = ["Playstation", "Xbox", "Computer"];
+  // const handleSubmit = (e: any) => {
+   
+  // };
 
   return (
     <section className="form">
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSearch}>
         <h2>fill in the fields to find other players!</h2>
-        <InputField
-          label="User name"
-          placeholder="Put your name here"
-          required={true}
-          value={name}
-          changed={(value: any) => {
-            setName(value);
-          }}
-        />
         <InputField
           label="ID Activision"
           placeholder="Put your ID here"
           required={true}
-          value={ID}
+          value={username}
           changed={(value: any) => {
-            setID(value);
+            setUsername(value);
           }}
         />
         <DropdownList
           label="Platform"
           platform={platform}
           placeHolder="Select your platform"
-          changed={(value: any) => setTeam(value)}
+          changed={(value: any) => setPlatform(value)}
         />
         <Button>Register</Button>
       </form>
+      {users.map(user => (
+        <div key={user.username}>
+          <h2>{user.username}</h2>
+          <p>Platform: {user.platform}</p>
+          <p>Level: {user.level}</p>
+          <p>K/D Ratio: {user.kdRatio.toFixed(2)}</p>
+          <p>Wins: {user.wins}</p>
+          <p>Games Played: {user.gamesPlayed}</p>
+          <p>Win Percentage: {(user.winPercentage * 100).toFixed(2)}%</p>
+        </div>
+      ))}
     </section>
   );
 };
